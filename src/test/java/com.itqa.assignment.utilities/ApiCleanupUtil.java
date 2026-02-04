a0392f20f57af910a46c077b675f5737f01ade0b
Navodh Fernando <Navodh123@outlook.com>
2026-02-04 23:14:09 +0530

    add: api cleanup utility

---
+++ src/test/java/com.itqa.assignment.utilities/ApiCleanupUtil.java
@@ -0,0 +1,92 @@
+package com.itqa.assignment.utilities;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.itqa.assignment.utilities.ApiHelper.getJwtToken;
+
+public class ApiCleanupUtil {
+    private static final String PREFIX = "API_";
+
+    public void cleanUpCategory() {
+        // 2. Get the list of plants
+        Response listRes = RestAssured.given()
+                .baseUri(ConfigReader.getProperty("api.base.uri"))
+                .header("Authorization", "Bearer " + getJwtToken(ApiHelper.UserRole.ADMIN))
+                .get("/categories?page=0&size=2000&sort=id,desc");
+
+        // 3. Filter and Delete
+        List<Map<String, Object>> categories = listRes.jsonPath().getList("content");
+
+        if (categories != null) {
+            categories.stream()
+                    .filter(c -> c.get("name") != null && c.get("name").toString().startsWith(PREFIX))
+                    .forEach(this::deleteCategory);
+        }
+    }
+
+    private void deleteCategory(Map<String, Object> category) {
+        String id = category.get("id").toString();
+        RestAssured.given()
+                .baseUri(ConfigReader.getProperty("api.base.uri"))
+                .header("Authorization", "Bearer " + getJwtToken(ApiHelper.UserRole.ADMIN))
+                .delete("/categories/" + id);
+    }
+
+    public void cleanUpPlants() {
+        // 2. Get the list of plants
+        Response listRes = RestAssured.given()
+                .baseUri(ConfigReader.getProperty("api.base.uri"))
+                .header("Authorization", "Bearer " + getJwtToken(ApiHelper.UserRole.ADMIN))
+                .get("/plants?page=0&size=2000");
+
+        // 3. Filter and Delete
+        List<Map<String, Object>> plants = listRes.jsonPath().getList("content");
+
+        if (plants != null) {
+            plants.stream()
+                    .filter(p -> p.get("name") != null && p.get("name").toString().startsWith(PREFIX))
+                    .forEach(this::deletePlant);
+        }
+    }
+
+    private void deletePlant(Map<String, Object> plant) {
+        String id = plant.get("id").toString();
+        RestAssured.given()
+                .baseUri(ConfigReader.getProperty("api.base.uri"))
+                .header("Authorization", "Bearer " + getJwtToken(ApiHelper.UserRole.ADMIN))
+                .delete("/plants/" + id);
+    }
+
+    public void cleanUpSales() {
+        // 2. Get the list of plants
+        Response listRes = RestAssured.given()
+                .baseUri(ConfigReader.getProperty("api.base.uri"))
+                .header("Authorization", "Bearer " + getJwtToken(ApiHelper.UserRole.ADMIN))
+                .get("/sales?page=0&size=2000");
+
+        // 3. Filter and Delete
+        List<Map<String, Object>> sales = listRes.jsonPath().getList("content");
+
+        if (sales != null) {
+            sales.stream()
+                    .filter(s -> s.get("name") != null && s.get("name").toString().startsWith(PREFIX))
+                    .forEach(this::deleteSales);
+        }
+    }
+
+    private void deleteSales(Map<String, Object> sale) {
+        String id = sale.get("id").toString();
+        RestAssured.given()
+                .baseUri(ConfigReader.getProperty("api.base.uri"))
+                .header("Authorization", "Bearer " + getJwtToken(ApiHelper.UserRole.ADMIN))
+                .delete("/sales/" + id);
+    }
+
+    public void cleanUpInventory() {
+        DatabaseUtil.executeUpdate("delete from inventory");
+    }
+}
